e3ef0bbd6cd2a2aac7e77baaeac23b1c270014a1
Kristýna Tlapáková <tlapakova24k@gmail.com>
2023-10-01 19:04:19 +0200

    Přidání posluchačů na prvních 10 tlačítek

--- hra.js
+++ hra.js
@@ -1,1 +1,32 @@
 let currentPlayer = 'circle';
+
+document
+  .querySelector('button:nth-child(1)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(2)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(3)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(4)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(5)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(6)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(7)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(8)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(9)')
+  .addEventListener('click', resolvePlayerTurn);
+document
+  .querySelector('button:nth-child(10)')
+  .addEventListener('click', resolvePlayerTurn);
